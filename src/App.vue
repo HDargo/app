e0bbf492aad46b64e79a47ba2396fcc858fef50c
KJH <KJH>
2023-02-06 14:48:07 +0900

    테스트 컴포넌트 생성

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TestPage msg="testMsg"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import HelloWorld from './components/HelloWorld.vue'
+import TestPage from '@/components/TestPage.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    //HelloWorld
+    TestPage
+}
 }
 </script>
 
