f70b2fbed236242eb6143e4e41400126878df4ee
HDargo <argo001@naver.com>
2023-02-07 21:22:24 +0900

    로그아웃 상태일 때 사이드바 수정 axios 추가 및 proxy 설정

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from '@/router/router'
 import { loadFonts } from './plugins/webfontloader'
+import { Axios } from 'axios'
 
 loadFonts()
 
-createApp(App)
-  .use(vuetify)
-  .use(router)
-  .mount('#app')
+const app = createApp(App)
+app.use(vuetify)
+app.use(router)
+app.config.globalProperties.$axios = Axios; 
+app.mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
+  },
+  devServer: {
+    proxy: {
+        //proxy 요청을 보낼 api 시작 부분
+        '/api': {
+            target:'http://localhost:3000',
+            changeOrigin: true,
+        },
+    },
+    //header : {'Access-Control-Allow-Origin' : '*'}
   }
 })
